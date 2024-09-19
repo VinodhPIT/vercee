9319385dd16f6d59fe7385cdb48f0949919f471c
vinodh.mn@pitsolutions.com <git345@vino>
2024-09-19 15:24:31 +0530

    fwefewfwf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+ 
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import "@/styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname !== "/404") {
+      router.replace(`/${router.locale}${router.asPath}`);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </Head>
+
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
+
+export default MyApp;
